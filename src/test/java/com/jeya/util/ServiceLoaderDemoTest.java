5701edab80ea80153708169d9037035dba3bbabd
Jeyaganeshan Jeyatharsini <Jeyaganeshan.Jeyatharsini@cambio.lk>
2018-09-25 13:20:00 +0530

    Added test case for service loader

---
+++ src/test/java/com/jeya/util/ServiceLoaderDemoTest.java
@@ -0,0 +1,52 @@
+package com.jeya.util;
+
+import static org.fest.reflect.core.Reflection.method;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ServiceLoader;
+
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.testng.PowerMockTestCase;
+import org.testng.annotations.Test;
+
+import com.jeya.util.CPServiceImplOne;
+import com.jeya.util.CPServiceImplTwo;
+import com.jeya.util.ServiceLoaderDemo;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ServiceLoaderDemo.class)
+public class ServiceLoaderDemoTest extends PowerMockTestCase {
+	private ServiceLoaderDemo serviceLoaderDemo = new ServiceLoaderDemo();
+
+	private ServiceLoader mockServiceLoader;
+	
+	@Test
+	public void testIterate() {
+		mockServiceLoader = PowerMockito.mock(ServiceLoader.class);
+		CPServiceImplTwo cpServiceImplTwo = new CPServiceImplTwo();
+		CPServiceImplOne cpServiceImplOne = new CPServiceImplOne();
+
+		List<CPService> mutableList = new ArrayList<>();
+		mutableList.add(cpServiceImplTwo);
+		mutableList.add(cpServiceImplOne);
+		
+		List<CPService> immutableList = Collections.unmodifiableList(mutableList);
+		
+		when(mockServiceLoader.iterator()).thenReturn(immutableList.iterator());
+		CPService methodToTest = null;
+		try {
+			methodToTest = method("iterate").withReturnType(CPService.class).withParameterTypes(ServiceLoader.class)
+					.in(serviceLoaderDemo).invoke(mockServiceLoader);
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}
+		assertEquals(methodToTest.getClass(), CPServiceImplOne.class);
+	}
+}
